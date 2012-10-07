73af78edcb5b9c7018babf44763a2737b0abf7d4
PiotrB <piotrb@gmail.com>
2012-10-07 22:46:49 +0200

    Pierwsza wersja zawierająca kompilujący się kod

--- Dyplom/Dyplom/Form1.cs
+++ Dyplom/Dyplom/Form1.cs
@@ -6,6 +6,10 @@ namespace PrzepisanyDyplom
 {
     public partial class Form1 : Form
     {
+        int q, a, z, n, k, m, i, b, uciec, nrekranu, cr1;
+        char ch;
+        byte starynr, n1;
+
         public Form1()
         {
             InitializeComponent();
@@ -24,10 +28,137 @@ namespace PrzepisanyDyplom
             button1.Enabled = false;
             pictureBox1.Refresh();
             m_graphics = pictureBox1.CreateGraphics();
-            m_graphics.DrawLine(m_pen, 10, 10, 100, 100);
+            MainRoutine();
+            //m_graphics.DrawLine(m_pen, 10, 10, 100, 100);
             button1.Enabled = true;
         }
 
+        private void MainRoutine()
+        {
+            starynr = 0; cr1 = 14;
+            do{NAPIS();} while (true);
+        }
 
+        private void NAPIS()
+        {
+            char q;
+            int n, m;
+            pictureBox1.Refresh();
+            settextstyle(2,0,4);
+            n=1;
+            setfillstyle(1,11);
+            bar(130,100,400,170);
+            setfillstyle(1,15);
+            bar(130,170,400,180);
+            setcolor(0);
+            PISZ(140,170,"WYBIERZ ODPOWIEDNIE POLE I NACISqNIJ ENTER");
+            obwodka1(130,130,400,141,3);
+            do
+            {
+                
+
+            setcolor(0);
+             PISZ(140,110,"PROGRAM SYMULUJAaCY PRACEa NASTAWNI EL. 110kV");
+             PISZ(140,130,"DEMONSTRACJA ELEMENTOqW POqL POMIAROWYCH");
+             PISZ(140,141,"SYMULACJA  STANOqW  AWARYJNYCH");
+             PISZ(140,152,"KROqTKA INFORMACJA O PROGRAMIE");
+                q = (char) 1;
+                // q=readkey;
+                // if q=#27 then pytanie;
+                // setfillstyle(1,7);
+                // if ord(q)<>0 then begin
+                // if q=#80 then n=N+1 else if q=#72 then n=n-1 else if (q<>#13)and(q<>#27) then beep;
+                // if n=4 then n=1;
+                // if n=0 then n=3;
+                // if n=1 then begin ry;obwodka1(130,130,400,141,3);end;
+                // if n=2 then begin ry;obwodka1(130,142,400,152,3);end;
+                // if n=3 then begin ry;obwodka1(130,151,400,163,3);end;
+                // end; setfillstyle(1,11);
+            } while (q!=13);
+            
+            // case n of
+            // 1:pokaz;
+            // 2:begin n1=1;wybor_awarii;end;
+            // 3:informacja;
+        }
+
+        private void obwodka1(int prx1,int pry1, int prx2, int pry2, int color)
+        {
+            // var  p:pointer;
+            // size:word;
+            // {
+            // size=imagesize(prx1,pry1,prx2,pry2);
+            // getmem(p,size);
+            // getimage(prx1,pry1,prx2,pry2,p^);
+            // setfillstyle(1,color);
+            // bar(prx1,pry1,prx2,pry2);
+            // setcolor(15);
+            // putimage(prx1,pry1,p^,andput);
+            // freemem(p,size);
+            // };
+            throw new NotImplementedException();
+        }
+
+        private void PISZ(int wsx, int wsy, string napis)
+        {
+            int c;
+            string znakp;
+            c = napis.Length;
+            for (int i = 1; i < c; i++)
+            {
+                znakp=napis.Substring(i,1);
+                if (znakp=="q") pzcoz(wsx - 3, wsy + 1);
+                else if (znakp=="l") pzl(wsx - 7, wsy + 8); 
+                else if (znakp=="z") pzz(wsx - 3, wsy + 0);
+                else if (znakp=="a") pzae(wsx - 2, wsy + 10);
+                else {outtextxy(wsx,wsy,znakp);wsx=wsx+6;}
+
+            }
+        }
+
+        private void outtextxy(int wsx, int wsy, string znakp)
+        {
+            throw new NotImplementedException();
+        }
+
+        private void pzae(int i1, int i2)
+        {
+            throw new NotImplementedException();
+        }
+
+        private void pzz(int i1, int i2)
+        {
+            throw new NotImplementedException();
+        }
+
+        private void pzl(int i1, int i2)
+        {
+            throw new NotImplementedException();
+        }
+
+        private void pzcoz(int i1, int i2)
+        {
+            throw new NotImplementedException();
+        }
+
+        private void setcolor (int color) 
+        {
+            throw new NotImplementedException();
+        }
+
+        private void bar (int left, int top, int right, int bottom)
+        {
+            throw new NotImplementedException();
+        }
+
+        private void setfillstyle(int pattern, int color)
+        {
+            throw new NotImplementedException();
+        }
+
+        private void settextstyle(int font, int direction, int charsize)
+        {
+            throw new NotImplementedException();
+        }
     }
 }
